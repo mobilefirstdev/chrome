b756ea0a6e61e0e069067f47b62e0f83f119d7c3
Andrew Cretin <andrew.cretin@gmail.com>
2021-06-23 15:00:07 -0600

    Updated default timeout to 60s

--- src/config.ts
+++ src/config.ts
@@ -95,7 +95,7 @@ const expandedDir = untildify(process.env.WORKSPACE_DIR || '');
 // Timers/Queue/Concurrency
 export const CONNECTION_TIMEOUT: number = parseNumber(
   process.env.CONNECTION_TIMEOUT,
-  30000,
+  60000,
 );
 export const MAX_CONCURRENT_SESSIONS: number = parseNumber(
   process.env.MAX_CONCURRENT_SESSIONS,
